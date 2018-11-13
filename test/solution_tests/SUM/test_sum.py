7d16dccbca34984ecc5a9bc7f99cd93903730407
sbx_user1060 <sbx_user1060@ip-10-129-157-158.eu-west-2.compute.internal>
2018-11-13 23:40:29 +0000

    Tue Nov 13 23:40:29 UTC 2018

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,9 +7,11 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum_solution.compute(1, 2), 3)
 
-    """Should add exception testing later, let's get the code running"""
     def test_sum_float(self):
         self.assertRaises(TypeError)
+
+    def test_param_out_of_scope(self):
+        self.assertRaises(ValueError)
 
 
 if __name__ == '__main__':
