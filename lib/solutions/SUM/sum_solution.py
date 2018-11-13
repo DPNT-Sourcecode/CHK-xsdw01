90977cf8ad0ac672fdab84ec1da3b5c3db710f66
sbx_user1060 <sbx_user1060@ip-10-129-157-158.eu-west-2.compute.internal>
2018-11-13 23:49:29 +0000

    Tue Nov 13 23:49:29 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if isinstance(x, int) and isinstance(y, int):
-        return x + y
-    else:
+    if not isinstance(x, int) and isinstance(y, int):
         raise TypeError('Both inputs should be integers')
+    return x + y
