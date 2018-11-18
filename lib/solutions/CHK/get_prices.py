1f1f0787f7aec4d0b925d5b88750144cc606491c
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 19:25:15 +0000

    Sun Nov 18 19:25:15 UTC 2018

--- lib/solutions/CHK/get_prices.py
+++ lib/solutions/CHK/get_prices.py
@@ -1,1 +1,5 @@
-filepath = '~/challenge/tdl-runner-python/challenges/CHK_R4.txt'
+filepath = '/Users/MichaelHook/challenge/tdl-runner-python/\
+            challenges/CHK_R4.txt'
+fh = open(filepath)
+for line in fh:
+    print line
