c7a1021b79a60bc064857bc85564dd7f345ea337
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 19:31:15 +0000

    Sun Nov 18 19:31:15 UTC 2018

--- lib/solutions/CHK/get_prices.py
+++ lib/solutions/CHK/get_prices.py
@@ -1,8 +1,10 @@
 import os
 
 
-filepath = os.path('/Users/MichaelHook/challenge/tdl-runner-python/\
-                    lib/solutions/CHKprice_list.txt')
+file_dir = os.path.dirname(os.path.realpath('__name__'))
+print file_dir
+
+# filepath = 'price_list.txt'
 fh = open(filepath)
 for line in fh:
     print line
