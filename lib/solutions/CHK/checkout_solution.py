7726fc72586b814dc0483dd893bda879f67b2281
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 18:24:50 +0000

    Sun Nov 18 18:24:50 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,14 +13,14 @@ def checkout(skus):
     """
 
     def _multibuy(items, discount):
-        num_deals = items /discount
+        num_deals = items / discount
         return (num_deals, items - (num_deals * discount))
 
     def _double_multibuy(items, discount_low, discount_high):
         num_deals_high = items / discount_high
         remaining = items - discount_high * num_deals_high
         num_deals_low = remaining / discount_low
-        remaining = items - discount_low * num_deals_low
+        remaining = remaining - discount_low * num_deals_low
         return (num_deals_high, num_deals_low, remaining)
 
     def _bogof(items, discounted_items, threshold):
@@ -32,22 +32,20 @@ def checkout(skus):
         remaining_discounted_items = discounted_items - num_deals
         return (num_deals, remaining_items, remaining_discounted_items)
 
-    def _check_off_items_in_deal(rem_basket, item_key, num_items):
-        rem_basket[item_key] = rem_basket[item_key] - num_items
-        if rem_basket[item_key] < 0:
-            print "Have removed more items from deals than existed"
-            raise ValueError
-        return rem_basket
+    # def _check_off_items_in_deal(rem_basket, item_key, num_items):
+    #     rem_basket[item_key] = rem_basket[item_key] - num_items
+    #     if rem_basket[item_key] < 0:
+    #         print "Have removed more items from deals than existed"
+    #         raise ValueError
+    #     return rem_basket
 
     def _check_for_deals(basket):
         remaining_basket = basket.copy()
         deals = dict()
 
-        deals['5A'], deals['3A'],
-        remaining_basket = _double_multibuy(remaining_basket['A'], 3, 5)
-
-        remaining_basket['A'] -= 3*deals['3A']
-        remaining_basket['A'] -= 5*deals['5A']
+        deals['5A'], deals['3A'], remaining_basket['A'] = \
+            _double_multibuy(remaining_basket['A'], 3, 5)
+        print deals, remaining_basket
 
         deals['2B'] = _multibuy(remaining_basket['B'])
         print deals['B']
