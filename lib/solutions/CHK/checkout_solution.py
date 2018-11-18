446c757f224edfa25a36619039aa9877f11cc652
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 16:07:08 +0000

    Sun Nov 18 16:07:08 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """ This method returns:
+            checkout(String) -> Integer
+        Where:
+            - param[0] = a String containing the SKUs of all the products
+            in the basket
+            - @return = an Integer representing the total checkout value
+            of the items
+    """
+
+    if not isinstance(skus, basestring):
+        return -1
+
+    
