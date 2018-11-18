994fea8b0b6238616e4a4f7a65abfcdcb021a167
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 19:37:15 +0000

    Sun Nov 18 19:37:15 UTC 2018

--- lib/solutions/CHK/get_prices.py
+++ lib/solutions/CHK/get_prices.py
@@ -1,10 +1,6 @@
-import os
-
-
-file_dir = os.path.dirname(os.path.realpath('__name__'))
-print file_dir
-
-filepath = '/Users/MichaelHook/challenge/tdl-runner-python/lib/solutions/CHK/price_list.txt'
+filepath = '''/Users/MichaelHook/challenge/tdl-runner-python/
+              lib/solutions/CHK/price_list.txt'''
 fh = open(filepath)
 for line in fh:
-    print line
+    if line.startswith('|'):
+        print line
