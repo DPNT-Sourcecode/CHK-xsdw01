bdd63379b8c4500b9bcad5a0006c2806262d0ca2
sbx_user1060 <sbx_user1060@ip-10-128-134-92.eu-west-2.compute.internal>
2018-11-18 19:40:15 +0000

    Sun Nov 18 19:40:15 UTC 2018

--- lib/solutions/CHK/get_prices.py
+++ lib/solutions/CHK/get_prices.py
@@ -1,6 +1,9 @@
-filepath = '''/Users/MichaelHook/challenge/tdl-runner-python/
-              lib/solutions/CHK/price_list.txt'''
+filepath = ('/Users/MichaelHook/challenge/tdl-runner-python/' +
+            'lib/solutions/CHK/price_list.txt')
 fh = open(filepath)
+
+price_list = {}
 for line in fh:
     if line.startswith('|'):
-        print line
+        if not line.split()[1].startswith('Item'):
+            print line.split()[1]
